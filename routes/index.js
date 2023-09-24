619422d2a4d01862cb66a91ddf17d933d0e8aec0
edw17003 <edw17003@byui.edu>
2023-09-23 23:29:28 -0600

    Updated rest calls

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,4 @@ router.get('/:id', controller.getSingle);
 
 module.exports = router;
 
-// const contactsRouter = require('./contacts');
-// router.use('/contacts', contactsRouter);
-
 module.exports = router;
